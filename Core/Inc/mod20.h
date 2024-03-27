4be18291d6de822f67dff0c0b39dacdf801ffa61
kikuchi <smj320@gmail.com>
2024-03-27 09:18:19 +0900

    mod20のスケルトン追加

---
+++ Core/Inc/mod20.h
@@ -0,0 +1,16 @@
+//
+// Created by kikuchi on 2024/03/27.
+//
+
+#ifndef DRILL03_MOD20_H
+#define DRILL03_MOD20_H
+
+#include "main.h"
+
+#define MOD20_I2C_ADDR
+
+void mod20_Init(I2C_HandleTypeDef *hi2c1);
+void mod32_read(I2C_HandleTypeDef *hi2c1, uint8_t *buf, int len);
+void mod32_write(I2C_HandleTypeDef *hi2c1, uint8_t *buf, int len);
+
+#endif //DRILL03_MOD20_H
---
+++ Core/Src/mod20.c
@@ -0,0 +1,17 @@
+//
+// Created by kikuchi on 2024/03/27.
+//
+#include "mod20.h"
+
+void mod20_Init(I2C_HandleTypeDef *hi2c1)
+{
+
+}
+void mod32_read(I2C_HandleTypeDef *hi2c1, uint8_t *buf, int len)
+{
+
+}
+void mod32_write(I2C_HandleTypeDef *hi2c1, uint8_t *buf, int len)
+{
+
+}
